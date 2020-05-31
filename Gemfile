c67fbe4a43078ef05a4c4834265466ec8f8ae4bb
kithub1 <kzyiwt515@yahoo.co.jp>
2020-05-31 04:55:00 +0000

    Finish creating-login

--- Gemfile
+++ Gemfile
@@ -38,3 +38,4 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'email_validator', '~> 1.6'
 gem 'rails-i18n', '~> 5.1' 
+gem 'bcrypt', '~> 3.1.7'
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   resources :members do
     get "search", on: :collection
   end
+  
+  resource :session, only: [:create, :destroy]
 end
 
