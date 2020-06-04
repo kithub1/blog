15207991bd55ff63cc2dc1cd701646b1255184bb
kithub1 <kzyiwt515@yahoo.co.jp>
2020-06-04 13:37:52 +0000

    Finish strong-parameters

--- db/seeds/development/articles.rb
+++ db/seeds/development/articles.rb
@@ -1,7 +1,7 @@
 body =
-  "毎日コツコツと勉強。\n\n" +
-  "最初は全く分からなかったが、少しずつ理解出来てきた。\n\n" +
-  "継続することが大切なんだと痛感する。"
+  "内容ト\n\n" +
+  "内容" +
+  "内容"
 
 0.upto(9) do |idx|
   Article.create(
@@ -16,7 +16,7 @@ end
 0.upto(29) do |idx|
   Article.create(
     title: "Article#{idx+10}",
-    body: "blah, blah, blah...",
+    body: "perginate",
     released_at: 100.days.ago.advance(days: idx),
     expired_at: nil,
     member_only: false
--- db/seeds/development/entries.rb
+++ db/seeds/development/entries.rb
@@ -7,7 +7,7 @@ body =
   0.upto(9) do |idx|
     Entry.create(
       author: member,
-      title: "野球観戦#{idx}",
+      title: "ブログ#{idx}",
       body: body,
       posted_at: 10.days.ago.advance(days: idx),
       status: %w(draft member_only public)[idx % 3]
